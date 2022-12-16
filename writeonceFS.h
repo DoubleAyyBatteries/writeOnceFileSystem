1f25fed3944a9af5d146b4d902090d6df9e4d6cf
DoubleAyyBatteries <89277901+DoubleAyyBatteries@users.noreply.github.com>
2022-12-15 21:23:29 -0500

    Update writeonceFS.h

--- writeonceFS.h
+++ writeonceFS.h
@@ -8,8 +8,31 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <signal.h>
+#include <errno.h>
+#include <sys/mman.h>
+
+
 
 #define default_size 4194304
+#define quanta 1024
+
+typedef struct fileblock
+{
+    char filename[128];
+    int fd;        // file descriptor
+    int allperm;   // all permissions
+    int groupperm; // group permissions
+    int usrperm;   // all permissions
+
+    size_t dataSize; // internal data size. Always >= user specified data size
+    void *data;
+    // struct fileblock next;
+} fb;
+
+struct fileblock *next(struct fileblock *block);
+void initialize();
+void printFiles();
 
 // int wo_mount(char* filename, void* memoryAddress);
 // int wo_unmount(void* memoryAddress);
